3948d8a118779d7ebd830d3dbc4b42ae5b1b831e
jihane-dev <jihane.jait@uit.ac.ma>
2025-06-30 02:25:23 +0200

    modif

--- streamlit-app.py
+++ streamlit-app.py
@@ -47,11 +47,11 @@ st_lottie(lottie_animation, key="lottie-immuno", height=300)
 # === Inspirational References Section ===
 st.markdown("### 🔬 Pour s’inspirer")
 st.markdown("""
-    - 💬 * "La technologie ne remplacera jamais un grand enseignant, mais la technologie entre les mains d’un grand enseignant peut être transformante. "* — George Couros
+    - 💬 "La technologie ne remplacera jamais un grand enseignant, mais la technologie entre les mains d’un grand enseignant peut être transformante. " — George Couros
     
-    - 💬 * "La meilleure façon de prédire l’avenir, c’est de l’inventer."* — Alan Kay 
+    - 💬 "La meilleure façon de prédire l’avenir, c’est de l’inventer." — Alan Kay 
     
-    - 💬 * " Immuno-Fun n’est pas qu’une application, c’est un pont entre l’abstrait et la compréhension réelle. "* — Jihane Jait
+    - 💬 "Immuno-Fun n’est pas qu’une application, c’est un pont entre l’abstrait et la compréhension réelle. " — Jihane Jait
 """)
 
 st.markdown("---")
